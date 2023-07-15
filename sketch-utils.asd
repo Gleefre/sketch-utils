d6d0f2b56fee0c6014f7940edce2600dd527e817
Gleefre <varedif.a.s@gmail.com>
2023-07-15 15:55:03 +0200

    Put my stuff here

---
+++ sketch-utils.asd
@@ -0,0 +1,7 @@
+(defsystem "sketch-utils"
+  :description "Utilities for `sketch' library."
+  :version "1.0.0"
+  :author "Grolter <varedif.a.s@gmail.com>"
+  :license "Apache 2.0"
+  :depends-on ("sketch" "sketch-fit")
+  :components ((:file "sketch-utils")))
---
+++ sketch-utils.lisp
@@ -0,0 +1,54 @@
+(defpackage #:sketch-utils
+  (:use #:cl #:sketch #:sketch-fit)
+  (:export #:fit #:with-fit #:fit-point)
+  (:export #:with-translate
+           #:with-rotate
+           #:with-scale)
+  (:export #:filter-alpha)
+  (:export #:enable-scissor
+           #:disable-scissor
+           #:with-scissor))
+
+;; Basic with- macros for translate, rotate and scale sketch functions
+(defmacro with-translate ((dx dy) &body body)
+  `(with-current-matrix
+     (translate ,dx ,dy)
+     ,@body))
+
+(defmacro with-rotate ((angle &optional (cx 0) (cy 0)) &body body)
+  `(with-current-matrix
+     (rotate ,angle ,cx ,cy)
+     ,@body))
+
+(defmacro with-scale ((sx &optional sy (cx 0) (cy 0)) &body body)
+  `(with-current-matrix
+     (scale ,sx ,sy ,cx ,cy)
+     ,@body))
+
+;; colors
+
+(defun filter-alpha (color alpha)
+  (rgb (color-red color) (color-green color) (color-blue color)
+       alpha))
+
+;; scissors
+
+(defun enable-scissor (x y w h)
+  (gl:enable :scissor-test)
+  (destructuring-bind ((x1 y1) (x2 y2))
+      (list
+       (sketch::transform-vertex (list x (+ y h)) (sketch::env-model-matrix sketch::*env*))
+       (sketch::transform-vertex (list (+ x w) y) (sketch::env-model-matrix sketch::*env*)))
+    (let* ((height (sketch::sketch-height sketch::*sketch*))
+           (y1 (- height y1))
+           (y2 (- height y2)))
+      (gl:scissor x1 y1 (- x2 x1) (- y2 y1)))))
+
+(defun disable-scissor ()
+  (gl:disable :scissor-test))
+
+(defmacro with-scissor ((x y w h) &body body)
+  `(progn
+     (enable-scissor ,x ,y ,w ,h)
+     ,@body
+     (disable-scissor)))
