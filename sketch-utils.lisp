d6f7b5beb0f2071c93932c9df6d5b91809c1b7be
Gleefre <varedif.a.s@gmail.com>
2023-10-20 02:53:10 +0200

    Merge with sketch-fit

--- sketch-utils.lisp
+++ sketch-utils.lisp
@@ -1,5 +1,5 @@
 (defpackage #:sketch-utils
-  (:use #:cl #:sketch #:sketch-fit #:sketch-buttons)
+  (:use #:cl #:sketch #:sketch-buttons)
   (:export #:fit #:with-fit #:fit-point)
   (:export #:with-split)
   (:export #:with-translate
@@ -33,6 +33,45 @@
            #:bcircle))
 
 (in-package #:sketch-utils)
+
+;;; sketch-fit
+
+(defun fit (width height from-width from-height &optional (to-x 0) (to-y 0) (from-x 0) (from-y 0) max-scale)
+  (translate from-x from-y)
+  (let* ((scale (min (/ from-width width)
+                     (/ from-height height)
+                     (if max-scale max-scale 10000)))
+         (x-shift (/ (- from-width (* width scale)) 2))
+         (y-shift (/ (- from-height (* height scale)) 2)))
+    (translate x-shift y-shift)
+    (scale scale))
+  (translate (- to-x) (- to-y)))
+
+(defun fit-point (x y width height from-width from-height
+                  &optional (to-x 0) (to-y 0) (from-x 0) (from-y 0) max-scale)
+  (setf x (- x from-x)
+        y (- y from-y))
+  (let* ((scale (min (/ from-width width)
+                     (/ from-height height)
+                     (if max-scale max-scale 10000)))
+         (x-shift (/ (- from-width (* width scale)) 2))
+         (y-shift (/ (- from-height (* height scale)) 2)))
+    (setf x (- x x-shift)
+          y (- y y-shift))
+    (setf x (/ x scale)
+          y (/ y scale)))
+  (setf x (+ x to-x)
+        y (+ y to-y))
+  (list x y))
+
+(defmacro with-fit ((width height from-width from-height
+                     &optional  (to-x 0) (to-y 0) (from-x 0) (from-y 0) max-scale)
+                    &body body)
+  `(progn
+     (push-matrix)
+     (fit ,width ,height ,from-width ,from-height ,to-x ,to-y ,from-x ,from-y ,max-scale)
+     ,@body
+     (pop-matrix)))
 
 ;;; with-split macro
 (defmacro with-split ((width-var height-var &optional (orientation :horizontal))
